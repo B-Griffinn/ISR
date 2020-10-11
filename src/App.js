efa385d48904b6f773bcdcc157d1d2ee77d38d19
Benjamin Griffin <49883273+B-Griffinn@users.noreply.github.com>
2020-10-10 17:07:22 -0700

    Development (#1)
    * Beginning of Navbar
    
    * Nav bar and router set up in APP.js
    
    * Navbar foundation created:
    no styles
    button begins now
    
    * Button Created
    
    * Hero Page added
    Landing page is showing properly
    
    * Display Syling for mobile ready
    
    * Home page now has working clickable cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navabar from "./componnets/NavBar/Navbar";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './componnets/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navabar />
+
+      <Switch>
+        <Route path="/" exact component={Home}/>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ src/componnets/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../Hero';
+import Cards from '../Cards';
+function Home () {
+    return (
+        <>
+        <HeroSection />
+        <Cards />
+        </>
+    )
+}
+
+export default Home;
