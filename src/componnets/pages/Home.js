db7ba7a743c223df0dd1aafd98d9c2a1ae60e900
Ben <bigriffin94@outlook.com>
2020-10-10 17:06:01 -0700

    Home page now has working clickable cards

--- src/componnets/pages/Home.js
+++ src/componnets/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../Hero';
-
+import Cards from '../Cards';
 function Home () {
     return (
         <>
         <HeroSection />
+        <Cards />
         </>
     )
 }
