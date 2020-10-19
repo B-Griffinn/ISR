3c89380957fdfb994ead57ce8230c59ee0e6dd53
Benjamin Griffin <49883273+B-Griffinn@users.noreply.github.com>
2020-10-19 11:34:48 -0700

    Development (#12)
    * Beginning of Navbar
    
    * Nav bar and router set up in APP.js
    
    * Navbar foundation created:
    no styles
    button begins now
    
    * Button Created
    
    * Hero Page added
    Landing page is showing properly
    
    * Display Syling for mobile ready
    
    * Home page now has working clickable cards
    
    * added microphone icon
    
    * Organized Folders
    
    * Organization Complete
    
    * Form Work
    
    * Contact form and styling are showing properly
    
    * Contact Page Working:
    still need to connect to emailJS
    
    * Mobile Queries added for contat page
    
    * Contact Page mobile styling check
    
    * Contact Form is almost perfect
    
    * Contact Form page is almost mobile friendly
    
    * Merch Page Now
    
    * Merch page is presentable but needs content
    
    * Notes added
    
    * FormSuccess has been udpated with loading message
    
    * Merch content ready for Modal
    
    * Media Screens
    
    * EmailJS installed
    sendEmail functionality working properly
    
    * Footer Component:
    footer added and styled
    edited for IRS needs
    ready to push and merge with main

---
+++ notes/presentation.md
@@ -0,0 +1,7 @@
+# Presentation
+
+> - basic landing page 
+> - basic contact page that is hooked up with an email service that allows 200 free emails a month
+> - merch is just an idea we need to work on together
+> - working social medias are IG and Spotify
+
--- src/componnets/pages/Home.js
+++ src/componnets/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import LandingPage from '../LandingPage/LandingPage';
 import Cards from '../cards/Cards';
+import Footer from '../footer/Footer';
 
 // Home Page is the Landing Page aka route to "/"
 function Home () {
@@ -10,6 +11,7 @@ function Home () {
         <>
         <LandingPage />
         <Cards />
+        <Footer/>
         </>
     )
 }
